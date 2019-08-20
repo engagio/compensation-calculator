0727cbd09104e881b889b07eb02d3a0262063716
Shaun VanWeelden <shaun@engagio.com>
2019-08-19 22:18:55 -0700

    Engagio-ify Everything

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 var port = process.env.PORT || 2676;
 app.listen(port, function () { console.log('App running on port', port); });
